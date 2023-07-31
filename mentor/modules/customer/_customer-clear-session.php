1b3fe7987ce77c6ed771dbc4700f99cb54465724
bcat95 <bcat9505@gmail.com>
2023-07-31 09:24:29 +0700

    230731_03

--- mentor/modules/customer/_customer-clear-session.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once("../../inc/includes.php");
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
-    unset($_SESSION["id_customer"]);
-    header("Location:".$base_url."/");
-    exit;
-?>
--- mentor/php/key.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once(__DIR__ . '/../inc/includes.php');
-// Set up API key
-$API_KEY = $config->openai_api_key;
